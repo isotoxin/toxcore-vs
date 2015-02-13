08a44a769760ccf6b36814daa9a3aefb0a0fb8aa
Rotkaermota <isotoxin.dev@yandex.ru>
2015-02-13 14:50:47 +0300

    prevent libsodium update version.h, no more need to define GARBAGE

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # toxcore-vs 
 
-How to build:
-1. Define GARGBAGE environment variable (path to any folder - all temprary stuff and builded libraries will be stored here)
-2. Install Visual Studio 2013 Express (if not yet)
-3. run build_libs.cmd
+How to build:<br />
+1. Install Visual Studio 2013 Express (if not yet)<br />
+2. run build_libs.cmd<br />
 
-Another way - create solution and add project (*.vcxproj) files, then build from VS gui
+Another way - create solution and add project (*.vcxproj) files, then build from msvs gui
 
 PS. There are yasm64.exe in vpx folder - it necessary to build asm files of vpx lib. So, if you use 32 bit windows, you have to download yasm32 here: http://yasm.tortall.net/Download.html
--- build_libs.cmd
+++ build_libs.cmd
@@ -1,24 +1,24 @@
+@echo off
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 if "aa%VS120COMNTOOLS%" == "aa" goto novs120
-if "aa%GARBAGE%" == "aa" goto nogarb
-
+if NOT "aa%GARBAGE%" == "aa" goto garbok
+echo warning: no ^"GARBAGE^" variable defined (path to temp compile files, must not be inside %TEMP%)
+set GARBAGE=%cd%\~garbage
+md %GARBAGE%
+:garbok
+echo ^"GARBAGE^" is %GARBAGE%
 set vsc="%VS120COMNTOOLS%..\..\vc\bin"
 set msb="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 
+%msb% toxcore\vs\toxcore.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Debug;GARBAGE=%GARBAGE%
+%msb% toxcore\vs\toxcore.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release;GARBAGE=%GARBAGE%
 
-
-%msb% toxcore\vs\toxcore.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Debug
-%msb% toxcore\vs\toxcore.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release
-
-%msb% opus\opus.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release
-%msb% libsodium\libsodium.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release
-%msb% libvpx\libvpx.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release
+%msb% opus\opus.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release;GARBAGE=%GARBAGE%
+%msb% libsodium\libsodium.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release;GARBAGE=%GARBAGE%
+%msb% libvpx\libvpx.vcxproj /fl1 /clp:ErrorsOnly /m:3 /t:Rebuild /p:Configuration=Release;GARBAGE=%GARBAGE%
 
 goto oka
 :novs120
 echo error: Visual Studio 2013 not found (install Visual Studio 2013 Express for free)
-goto oka
-:nogarb
-echo error: no ^"GARBAGE^" variable defined (path to temp compile files, must not be inside %TEMP%)
 :oka
