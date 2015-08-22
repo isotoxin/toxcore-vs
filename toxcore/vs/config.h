cbc13fe75364b98ecdfd5ee3d0bbafa0a74b64b3
Rotkaermota <isotoxin.dev@yandex.ru>
2015-08-22 16:40:36 +0300

    sprintf

--- toxcore/vs/config.h
+++ toxcore/vs/config.h
@@ -7,7 +7,9 @@
 
 typedef int ssize_t;
 
-//#define snprintf _snprintf
+#if _MSC_VER <= 1800
+#define snprintf _snprintf
+#endif
 
 #define DYNAMIC( eltype, arrname, arrsize ) size_t sizeof_##arrname = sizeof(eltype) * (arrsize); eltype * arrname = _alloca( sizeof_##arrname )
 #define sizeOf( dynarrname ) sizeof_##dynarrname
